6bea8bfbd5f48995bc745e8bf66f82d2c5891ff5
Alex Untilov <93001397+Crypto0w0@users.noreply.github.com>
2023-02-06 19:01:45 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class Student{
+    string name;
+    int* rates = new int[100];
+public:
+    Student(){
+        name = nullptr;
+        rates = nullptr;
+    }
+    ~Student(){
+        delete[] rates;
+    }
+    Student(const Student& s){
+        this->name = s.name;
+        for (int i = 0; i < 100; i++){
+            this->rates[i] = s.rates[i];
+        }
+    }
+    int* getRates(){
+        int* r = new int[100];
+        for (int i = 0; i < 100; i++){
+            this->rates[i] = r[i];
+        }
+        return r;
+    }
+    Student& operator=(Student& s){
+        s.name = this->name;
+        s.rates = getRates();
+        delete getRates();
+        return s;
+    }
+};
+
+int main() {
+    Student s1;
+    Student s2;
+    s1 = s2;
+}
